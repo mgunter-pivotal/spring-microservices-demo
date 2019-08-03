76b551a5ac8a5bbecf7c15dfa5087b2ad6c4311a
mgunter-pivotal <mgunter@pivotal.io>
2019-08-03 14:22:36 -0400

    external routes

--- add-network-policy.sh
+++
@@ -1,4 +0,0 @@
- cf add-network-policy gateway --destination-app time --port 8080 --protocol tcp
- cf add-network-policy gateway --destination-app echo --port 8080 --protocol tcp
- cf add-network-policy gateway --destination-app whoami --port 8080 --protocol tcp
- cf add-network-policy gateway --destination-app greeting --port 8080 --protocol tcp
---
+++ clean-up.bat
@@ -0,0 +1,5 @@
+cf delete gateway
+cf delete echo
+cf delete time
+cf delete whoami
+cf delete greeting
